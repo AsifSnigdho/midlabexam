3dbe78af1e1db78aaa19e84a1922cbefa946051c
AsifSnigdho <60178117+AsifSnigdho@users.noreply.github.com>
2020-03-11 15:25:59 +0600

    Add files via upload

---
+++ labExam2/changepassword.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>change password</title>
+</head>
+<body>
+	<h1></h1>
+
+
+	<form>
+	<fieldset>
+		<legend>Change password</legend>
+		<table>
+		<tr>
+			<td>Current Password <br/> <input type="password" name="" value=""/></td>
+			
+		</tr>
+		<tr>
+			<td>Password <br/> <input type="password" name="" value=""/></td>
+			
+		</tr>
+		<tr>
+			<td>Retype new Password <br/> <input type="password" name="" value=""/></td>
+			
+		</tr>
+		<tr>
+			
+			<td>
+				
+				<input type="submit" name="" value="Change"> <a href="Login.php">Home</a>
+				
+			</td>
+		</tr>
+		</table>
+	</fieldset>
+	</form>
+</body>
+</html>
---
+++ labExam2/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in form</title>
+</head>
+<body>
+	<h1></h1>
+
+
+	<form>
+	<fieldset>
+		<legend>LOGIN</legend>
+		<table>
+		<tr>
+			<td>User Id <br/><input type="text" name="" value="" size="20" /></td>
+			
+		</tr>
+		<tr>
+			<td>Password <br/> <input type="password" name="" value=""/></td>
+			
+		</tr>
+		<tr>
+			
+			<td>
+				
+				<input type="submit" name="" value="Login"> <a href="registration.php">Register</a>
+				
+			</td>
+		</tr>
